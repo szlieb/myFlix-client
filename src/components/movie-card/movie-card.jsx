688124950e9994f4b879fee6c315edc9e72ab519
Zalmy L <shneurlieberman@gmail.com>
2023-03-14 19:00:39 -0400

    3.4 init commit with props and stuff

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,20 @@
+import PropTypes from "prop-types";
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
+        <div
+            onClick={() => {
+                onMovieClick(movie);
+            }}
+        >
+            {movie.title}
+        </div>
     );
-  };
+};
 
-  
+// Define the props constraints for BookCard
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,24 +1,43 @@
 export const MovieView = ({ movie, onBackClick }) => {
-    return (
+  return (
       <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.genre}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
+          <div>
+              <img src={movie.image} />
+          </div>
+          <div>
+              <span>Title: </span>
+              <span>{movie.title}</span>
+          </div>
+          <div>
+              <span>Genre: </span>
+          </div>
+
+          <div>
+              <span>Name: </span>
+              <span>{movie.genre.Name}</span>
+          </div>
+
+          <div>
+              <span>Description: </span>
+              <span>{movie.genre.Description}</span>
+          </div>
+
+          <div>
+              <span>Director: </span>
+          </div>
+          <div>
+              <span>Name: </span>
+              <span>{movie.director.Name}</span>
+          </div>
+          <div>
+              <span>Bio: </span>
+              <span>{movie.director.Bio}</span>
+          </div>
+          <div>
+              <span>Description: </span>
+              <span>{movie.description}</span>
+          </div>
+          <button onClick={onBackClick}>Back</button>
       </div>
-    );
-  };
-  
-  
+  );
+};
